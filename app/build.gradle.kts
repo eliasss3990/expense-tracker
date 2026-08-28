cd961482d21980385321deca76f3f8cea4d91744
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-08-28 16:04:25 -0300

    chore: bump version a 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.eliasgonzalez.expensetracker"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.3.0"
+        versionCode = 3
+        versionName = "0.3.1"
     }
 
     buildFeatures {
